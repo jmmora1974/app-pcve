7e2846dceeef47dd3729786a889bcdf6ed971158
jmmora1974 <jmmora1974@gmail.com>
2024-11-18 22:07:25 +0100

    modified:   src/app/pages/previsiones/modal-serv-generales/modal-serv-generales.page.html modified:   src/app/pages/previsiones/modal-serv-generales/modal-serv-generales.page.ts modified:   src/app/pages/previsiones/moldal-vivienda/moldal-vivienda.page.html

--- src/app/pages/previsiones/modal-serv-generales/modal-serv-generales.page.ts
+++ src/app/pages/previsiones/modal-serv-generales/modal-serv-generales.page.ts
@@ -1,10 +1,10 @@
 
 import { JsonPipe } from '@angular/common';
-import {ChangeDetectionStrategy, Component, signal, OnInit, input, Input} from '@angular/core';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-import {MatSelectModule} from '@angular/material/select';
+import { Component, signal, OnInit,  WritableSignal} from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
 import { IAscensor } from 'src/app/models/iascensor';
+
 
 @Component({
   selector: 'app-modal-serv-generales',
@@ -15,18 +15,28 @@ export class ModalServGeneralesPage implements OnInit {
   submitted = false;
   public tipoPotAscensor : string[]=['ITA-1','ITA-2','ITA-3','ITA-4','ITA-5','ITA-6','OTRO'];
   public medidorPotencia: string[]=['kW','W','cV'];
- 
-  model:IAscensor;
+  formAscensor!: FormGroup;
+  modelAscensor!:IAscensor;
+  listaAscensores: WritableSignal<IAscensor[]> = signal<IAscensor[]>([]);
+
   //@Input()  numAsc:any;
 
 
-  constructor() { 
-    this.model={
+  constructor(private fbAsc:FormBuilder) { 
+    this.modelAscensor={
       id: 0,
       numAscensores: 2,
       tipoMotorAsc:'ITA-1',
       potenciaMotorAsc: 2
       } ;
+
+      this.formAscensor = this.fbAsc.group({
+        id: new FormControl(0, Validators.required),
+        numAscensores: new FormControl(0, Validators.required),
+        tipoMotorAsc: new FormControl('ITA-1', Validators.required),
+        potenciaMotorAsc: new FormControl(0, Validators.required),
+       
+      });
   }
 
   ngOnInit() {
@@ -34,7 +44,10 @@ export class ModalServGeneralesPage implements OnInit {
 
   agregarAscensor(){
     this.submitted = true; 
-    console.log(this.model);
+    console.log('Formascen ',this.formAscensor.value);
+    console.log('modelasc',this.modelAscensor);
+    this.modelAscensor.id=this.listaAscensores().length;
+    this.listaAscensores.update((values:IAscensor[])=>[...values,this.modelAscensor]);
    }
 
    agregarGrupoMotor(){
